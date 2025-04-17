1362fab256ae721813f58ee4190590496ca066ec
Mago Karin <jeissonvillaizan@gmail.com>
2025-04-17 02:19:10 -0500

    New: Creacion Clase ImprimirConstructoresCarrera

--- Formula1/src/main/java/edu/ucompensar/Main.java
+++ Formula1/src/main/java/edu/ucompensar/Main.java
@@ -9,11 +9,8 @@ public class Main {
         // Guardar datos de circuitos, carreras y resultados
         RecoleccionDatos.guardarDatosAPI("http://ergast.com/api/f1/2024/circuits.json", "circuitos_f1_2024.json");
         RecoleccionDatos.guardarDatosAPI("http://ergast.com/api/f1/2024/results.json", "resultados_f1_2024.json");
-        RecoleccionDatos.guardarDatosAPI("http://ergast.com/api/f1/2024/qualifying.json", "clasificacion_f1_2024.json");
         RecoleccionDatos.guardarDatosAPI("http://ergast.com/api/f1/2024/drivers.json", "conductores_f1_2024.json");
-        RecoleccionDatos.guardarDatosAPI("http://ergast.com/api/f1/2024/constructors.json", "constructores_f1_2024.json");
-      
-        
+        RecoleccionDatos.guardarDatosAPI("http://ergast.com/api/f1/2024/constructors.json", "constructores_f1_2024.json");        
         menu.menuPrincipal();
     }
 }
